0ab3418e4e248cc6bb41767f895b6524f7cdc339
xoquoil <xoquoil@gmail.com>
2024-10-10 19:34:19 +0900

    仮トップページ作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Rails.start()
 ActiveStorage.start()
