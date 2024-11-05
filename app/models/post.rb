2d07d73f12a8286e322c5797b9aaeb844ed91e46
xoquoil <xoquoil@gmail.com>
2024-11-05 20:52:40 +0900

    ログイン機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :pins, dependent: :destroy
   accepts_nested_attributes_for :pins, reject_if: :all_blank, allow_destroy: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
   resources :pins
+  resources :users
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
