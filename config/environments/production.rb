21fc7930fecca0ee13b00eab6d847908de794f48
xoquoil <xoquoil@gmail.com>
2024-11-21 18:31:53 +0900

    本番環境でパスワードリセット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'pinpoint-map.com', # 独自ドメインを指定
+    user_name: 'apikey',       # 固定値（"apikey"）
+    password: ENV['SENDGRID_API_KEY'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'pinpoint-map.com', protocol: 'https' }
+  config.action_mailer.default_options = { from: 'no-reply@pinpoint-map.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
