632af258d0959992d769a1ca931a9ef64723994e
xoquoil <xoquoil@gmail.com>
2024-12-05 18:40:34 +0900

    minimagickで画像サイズを統一

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'letter_opener_web', '2.0.0'
 gem 'config', '4.0.0'
 gem 'googleauth'
 gem "meta-tags"
+gem 'mini_magick'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.7', '>= 6.1.7.8'
 # Use mysql as the database for Active Record
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -7,13 +7,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  process resize_to_fill: [100, 100, "Center"]
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [300, 800]
+  def default_url(*args)
+      ActionController::Base.helpers.asset_path('sample.jpg')
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
