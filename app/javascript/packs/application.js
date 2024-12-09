bde8d9542cf6a966143bcd16b2957a5e2ba9183d
xoquoil <xoquoil@gmail.com>
2024-12-09 15:37:34 +0900

    saac修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "stylesheets/application";
 
 require("jquery")
 require("@nathanvda/cocoon")
